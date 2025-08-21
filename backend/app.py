5a82c55cd8800aa69f6a381e54d5a856bce14af5
Jack van Wijk <jack.vanwijk@hotmail.com>
2025-08-22 07:08:08 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import os
 import random
 import string
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, join_room, leave_room, emit
 
 # --- Flask app ---
@@ -10,7 +10,11 @@ app.secret_key = os.environ.get("FLASK_SECRET_KEY", "SUPER_SECRET_KEY")
 
 # --- Socket.IO setup ---
 socketio = SocketIO(app, cors_allowed_origins="*")
-active_rooms = {}  # {code: [usernames]}
+
+# Active rooms: {code: [usernames]}
+active_rooms = {}
+# Track which user is tied to which socket id
+user_sessions = {}  # {sid: {"username": str, "room": str}}
 
 @app.route("/")
 def index():
@@ -21,7 +25,10 @@ def index():
 def create_room(data):
     username = data.get("username", "Anonymous")
     code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
     active_rooms[code] = [username]
+    user_sessions[request.sid] = {"username": username, "room": code}
+
     join_room(code)
     send({"action": "room_created", "code": code}, room=request.sid)
     emit("members", active_rooms[code], room=code)
@@ -30,8 +37,11 @@ def create_room(data):
 def join_chat(data):
     code = data.get("code")
     username = data.get("username", "Anonymous")
+
     if code in active_rooms:
         active_rooms[code].append(username)
+        user_sessions[request.sid] = {"username": username, "room": code}
+
         join_room(code)
         send({"action": "joined_room", "code": code}, room=request.sid)
         emit("members", active_rooms[code], room=code)
@@ -48,10 +58,22 @@ def handle_message(msg):
 
 @socketio.on("disconnect")
 def handle_disconnect():
-    for code, members in list(active_rooms.items()):
-        # Remove user from room if known
-        # (We can improve by tracking usernames per session)
-        pass  # Simplified, not removing for now
+    sid = request.sid
+    if sid in user_sessions:
+        info = user_sessions[sid]
+        username = info["username"]
+        room = info["room"]
+
+        if room in active_rooms and username in active_rooms[room]:
+            active_rooms[room].remove(username)
+
+            # If room is empty, delete it
+            if not active_rooms[room]:
+                del active_rooms[room]
+            else:
+                emit("members", active_rooms[room], room=room)
+
+        del user_sessions[sid]
 
 # --- Run server ---
 if __name__ == "__main__":
